8c5348e13c25db4bd1fbf859d2ce270b7f66f1cf
Henrique Suel <h.suel17@hotmail.com>
2021-05-22 22:10:01 -0300

    Pagina preview do post

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,55 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
-import { query as q } from 'faunadb'
+
 import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
-    // Configure one or more authentication providers
     providers: [
         Providers.GitHub({
             clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRETS,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
             scope: 'read:user'
         }),
     ],
     callbacks: {
+        async session(session) {
+            try {
+                const userActiveSubscription = await fauna.query(
+                    q.Get(
+                        q.Intersection([
+                            q.Match(
+                                q.Index('subscription_by_user_ref'),
+                                q.Select(
+                                    "ref",
+                                    q.Get(
+                                        q.Match(
+                                            q.Index('user_by_email'),
+                                            q.Casefold(session.user.email)
+                                        )
+                                    )
+                                )
+                            ),
+                            q.Match(
+                                q.Index('subscription_by_status'),
+                                "active"
+                            )
+                        ])
+                    )
+                )
+
+                return {
+                    ...session,
+                    activeSubscription: userActiveSubscription
+                }
+            } catch {
+                return {
+                    ...session,
+                    activeSubscription: null,
+                }
+            }
+        },
         async signIn(user, account, profile) {
             const { email } = user
 
@@ -38,13 +75,12 @@ export default NextAuth({
                             )
                         )
                     )
-
                 )
+
                 return true
             } catch {
                 return false
             }
-
-        }
+        },
     }
 })
